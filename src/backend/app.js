6cff4ce256debe188c757bec9f2b8250d0011971
TheSkinnyRat <purwa.sabrang@gmail.com>
2021-11-21 23:49:36 +0700

    initializing backend folder and file structure

---
+++ src/backend/app.js
@@ -0,0 +1,13 @@
+// const express = require('express');
+// const app = express();
+
+// app.use(express.urlencoded({extended: false}));
+// app.use(express.json());
+
+// const appRoute = require('./routes/');
+// app.use('/', appRoute);
+
+// const port = process.env.APP_PORT || 8080;
+// app.listen(port, ()=>{
+//   console.log(`Server is running on port ${port}.`);
+// });
