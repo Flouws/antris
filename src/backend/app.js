e1d163f0d8fc1dae83a5f76bd7621ca56c4ca3e4
TheSkinnyRat <purwa.sabrang@gmail.com>
2021-11-22 05:17:57 +0700

    set backend to serve static file in dist folder

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,13 +1,16 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
+const path = require('path');
 
-// app.use(express.urlencoded({extended: false}));
-// app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.use('/', express.static(path.join(__dirname, '../../dist')));
 
 // const appRoute = require('./routes/');
 // app.use('/', appRoute);
 
-// const port = process.env.APP_PORT || 8080;
-// app.listen(port, ()=>{
-//   console.log(`Server is running on port ${port}.`);
-// });
+const port = process.env.APP_PORT || 8080;
+app.listen(port, ()=>{
+  console.log(`Server is running on port ${port}.`);
+});
