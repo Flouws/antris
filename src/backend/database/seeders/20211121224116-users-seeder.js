0a38959c59711276a0360489ff72bde3aa58a54b
TheSkinnyRat <purwa.sabrang@gmail.com>
2021-11-22 07:29:22 +0700

    initializing database

---
+++ src/backend/database/seeders/20211121224116-users-seeder.js
@@ -0,0 +1,34 @@
+/* eslint-disable max-len */
+'use strict';
+const bcrypt = require('bcrypt');
+const {dbConfig} = require('../../config/dbConfig');
+
+const hashPassword = (password) => {
+  return bcrypt.hashSync(password, dbConfig.ROUND_SALT);
+};
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return await queryInterface.bulkInsert('users', [{
+      uuid: 'd4870a0a-a235-40c8-91c3-8f213a3f9dfb',
+      name: 'user',
+      email: 'user@email.com',
+      password: hashPassword('user'),
+      address: null,
+      picture: null,
+      roleId: 1,
+    }, {
+      uuid: 'b841cebb-1e0d-4974-aba4-82f4618c714a',
+      name: 'hospital',
+      email: 'hospital@email.com',
+      password: hashPassword('hospital'),
+      address: 'Jl. Mangga, Komplek Durian, Desa Mars, Kec Venus, Kota Bumi. ',
+      picture: null,
+      roleId: 2,
+    }]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return await queryInterface.bulkDelete('users', null, {});
+  },
+};
