630c5dcfa2d056acb7dc16f6d2c5d5d062c0429f
MrOMadness <nicholas.stanciosaka@gmail.com>
2021-11-23 22:47:58 +0700

    delete login-page and register-page component

--- src/frontend/scripts/index.js
+++ src/frontend/scripts/index.js
@@ -1,8 +1,6 @@
 import 'regenerator-runtime';
 import 'bootstrap';
 import '../styles/main.scss';
-import './component/login-page.js';
-import './component/register-page.js';
 import './component/nav-bar.js';
 import '../styles/main.css';
 import App from './views/app.js';
