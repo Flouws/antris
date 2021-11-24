379f0345d3d941c85977e21cff6f7eb04a4f96ac
TheSkinnyRat <purwa.sabrang@gmail.com>
2021-11-24 08:24:52 +0700

    set jwt to not verify at controller

--- src/backend/controllers/user.controller.js
+++ src/backend/controllers/user.controller.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 const {baseResponse} = require('../base/index');
+const bcrypt = require('bcrypt');
 const db = require('../database/models');
 const User = db.users;
 const jwt = require('jsonwebtoken');
+
+const hashPassword = (password) => {
+  return bcrypt.hashSync(password, parseInt(process.env.APP_ROUND_SALT));
+};
 
 exports.getProfile = async (req, res) => {
   const token = req.headers['x-access-token'];
@@ -12,34 +17,29 @@ exports.getProfile = async (req, res) => {
   }
 
   try {
-    const decodedToken = jwt.verify(token, process.env.APP_KEY);
+    const decodedToken = jwt.decode(token);
 
-    try {
-      const user = await User.findOne({
-        where: {
-          uuid: decodedToken.uuid,
-        },
-      });
+    const user = await User.findOne({
+      where: {
+        uuid: decodedToken.uuid,
+      },
+    });
 
-      return baseResponse.ok(res, {
-        user: {
-          uuid: user.uuid,
-          name: user.name,
-          email: user.email,
-          address: user.address,
-          picture: user.picture,
-          createdAt: user.createdAt,
-          updatedAt: user.updatedAt,
-        },
-      });
-    } catch (error) {
-      return baseResponse.error(res, 500, error.message);
-    }
+    return baseResponse.ok(res, {
+      user: {
+        uuid: user.uuid,
+        name: user.name,
+        email: user.email,
+        address: user.address,
+        picture: user.picture,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      },
+    });
   } catch (error) {
-    return baseResponse.error(res, 401, error.message);
+    return baseResponse.error(res, 500, error.message);
   }
 };
-
 
 exports.home = (req, res) => {
   return baseResponse.ok(res, {
