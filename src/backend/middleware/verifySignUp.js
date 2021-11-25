b3025fe239996df0cb7e8ead0861380e381b796b
TheSkinnyRat <purwa.sabrang@gmail.com>
2021-11-26 02:45:01 +0700

    create register profile image upload

--- src/backend/middleware/verifySignUp.js
+++ src/backend/middleware/verifySignUp.js
@@ -2,19 +2,30 @@ const {baseResponse} = require('../base/index');
 const db = require('../database/models');
 const User = db.users;
 const Role = db.roles;
+const fs = require('fs');
 
 checkDuplicateEmail = async (req, res, next) => {
+  const deleteImage = () => {
+    if (req.file) {
+      fs.unlinkSync(req.file.path);
+    }
+  };
+
   const required = [
     'email',
   ];
-
   try {
     required.forEach((requiredItem) => {
       if (!req.body[requiredItem]) {
         throw new Error(`Should contain ${requiredItem}`);
       }
     });
+  } catch (error) {
+    deleteImage();
+    return baseResponse.error(res, 400, error.message);
+  }
 
+  try {
     const user = await User.findOne({
       where: {
         email: req.body.email,
@@ -22,26 +33,37 @@ checkDuplicateEmail = async (req, res, next) => {
     });
 
     if (user) {
+      deleteImage();
       return baseResponse.error(res, 400, 'Email is already in use.');
     }
   } catch (error) {
+    deleteImage();
     return baseResponse.error(res, 500, error.message);
   }
   next();
 };
 
 checkRoleExist = async (req, res, next) => {
+  const deleteImage = () => {
+    if (req.file) {
+      fs.unlinkSync(req.file.path);
+    }
+  };
   const required = [
     'roleId',
   ];
-
   try {
     required.forEach((requiredItem) => {
       if (!req.body[requiredItem]) {
         throw new Error(`Should contain ${requiredItem}`);
       }
     });
+  } catch (error) {
+    deleteImage();
+    return baseResponse.error(res, 400, error.message);
+  }
 
+  try {
     const user = await Role.findOne({
       where: {
         id: req.body.roleId,
@@ -49,9 +71,11 @@ checkRoleExist = async (req, res, next) => {
     });
 
     if (!user) {
+      deleteImage();
       return baseResponse.error(res, 400, 'Role id is not valid.');
     }
   } catch (error) {
+    deleteImage();
     return baseResponse.error(res, 500, error.message);
   }
   next();
--- src/backend/routes/auth.route.js
+++ src/backend/routes/auth.route.js
@@ -1,9 +1,13 @@
 /* eslint-disable new-cap */
 const router = require('express').Router();
 const {auth} = require('../controllers');
-const {verifySignUp} = require('../middleware');
+const {
+  verifySignUp,
+  upload,
+} = require('../middleware');
 
 router.post('/signup', [
+  upload.userProfileUpload,
   verifySignUp.checkDuplicateEmail,
   verifySignUp.checkRoleExist,
 ], auth.signup);
