384e6a612e542cefca641504ed2b474945c0e62d
TheSkinnyRat <purwa.sabrang@gmail.com>
2021-11-29 08:00:56 +0700

    initializing controller

--- src/backend/controllers/index.js
+++ src/backend/controllers/index.js
@@ -2,10 +2,12 @@ const home = require('./home.controller.js');
 const auth = require('./auth.controller.js');
 const user = require('./user.controller.js');
 const hospital = require('./hospital.controller.js');
+const hospitals = require('./hospitals.controller.js');
 
 module.exports ={
   home,
   auth,
   user,
   hospital,
+  hospitals,
 };
