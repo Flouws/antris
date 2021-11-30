07603eddcec4b1e1f4c11161dac81cd7e185582d
MrOMadness <nicholas.stanciosaka@gmail.com>
2021-11-30 21:39:08 +0700

    added error page

--- src/frontend/scripts/routes/routes.js
+++ src/frontend/scripts/routes/routes.js
@@ -2,13 +2,15 @@ import Login from '../views/pages/login';
 import Register from '../views/pages/register';
 import Dashboard from '../views/pages/dashboard';
 import Profile from '../views/pages/profile';
+import Error from '../views/pages/error';
 
 const routes = {
-  '/': Profile, // default page
+  '/': Profile, // default page TODO: Balikin ke login abis selesai develop
   '/login': Login,
   '/register': Register,
   '/dashboard': Dashboard,
   '/profile': Profile,
+  '/error': Error, // TODO: Connect error to bad routes
 };
 
 export default routes;
---
+++ src/frontend/scripts/views/pages/error.js
@@ -0,0 +1,27 @@
+const Error = {
+  async render() {
+    const errorType = '404';
+    const errorDesc = 'The page you are looking for was not found.';
+
+    $('nav-bar').empty(); // remove navbar
+
+    return `
+  <div class="d-flex flex-row align-items-center" id="error-page">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-12 text-center">
+          <span class="display-1 d-block">${errorType}</span>
+          <div class="mb-4 lead">${errorDesc}</div>
+          <a href="/" class="btn btn-link">Back to Home</a>
+        </div>
+      </div>
+    </div>
+  </div>
+      `;
+  },
+
+  async afterRender() {
+  },
+};
+
+export default Error;
