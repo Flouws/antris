0a5e4a823f733b704707516367b01c61cf09b4ac
TheSkinnyRat <purwa.sabrang@gmail.com>
2021-12-08 07:15:12 +0700

    add `appointment model`

---
+++ src/backend/database/models/appointments.js
@@ -0,0 +1,30 @@
+/* eslint-disable valid-jsdoc */
+/* eslint-disable require-jsdoc */
+'use strict';
+const {
+  Model,
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class appointments extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      appointments.belongsTo(models.polys);
+    }
+  };
+  appointments.init({
+    polyId: DataTypes.INTEGER,
+    day: DataTypes.INTEGER,
+    timeStart: DataTypes.TIME,
+    timeEnd: DataTypes.TIME,
+  }, {
+    sequelize,
+    modelName: 'appointments',
+    timestamps: true,
+  });
+  return appointments;
+};
