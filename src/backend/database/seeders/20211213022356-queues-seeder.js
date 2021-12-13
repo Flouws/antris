a74c2e0b411089a1f9f0a856b06730b23c2910fd
TheSkinnyRat <purwa.sabrang@gmail.com>
2021-12-13 09:31:17 +0700

    create `queues` seeder

---
+++ src/backend/database/seeders/20211213022356-queues-seeder.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return await queryInterface.bulkInsert('queues', [{
+      userId: 1,
+      appointmentId: 1,
+      date: '2021-12-12',
+      queue: 1,
+      queueStatusId: 1,
+    }, {
+      userId: 1,
+      appointmentId: 1,
+      date: '2021-12-12',
+      queue: 2,
+      queueStatusId: 1,
+    }]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return await queryInterface.bulkDelete('queues', null, {});
+  },
+};
