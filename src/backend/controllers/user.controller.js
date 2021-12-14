7de938db460cd97b7ef979717f39ed1c07829fd3
TheSkinnyRat <purwa.sabrang@gmail.com>
2021-12-15 01:34:35 +0700

    create `add queue` for user

--- src/backend/controllers/user.controller.js
+++ src/backend/controllers/user.controller.js
@@ -4,6 +4,10 @@ const {baseResponse} = require('../base/index');
 const bcrypt = require('bcrypt');
 const db = require('../database/models');
 const User = db.users;
+const Poly = db.polys;
+const Appointment = db.appointments;
+const Queue = db.queues;
+const {Op} = require('sequelize/dist');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const path = require('path');
@@ -117,6 +121,136 @@ exports.deleteProfile = async (req, res) => {
       message: 'Profile deleted successfully.',
     });
   } catch (error) {
+    return baseResponse.error(res, 500, error.message);
+  }
+};
+
+exports.addQueue = async (req, res) => {
+  const deleteImage = () => {
+    if (req.files.picture1) {
+      fs.unlinkSync(req.files.picture1[0].path);
+    }
+    if (req.files.picture2) {
+      fs.unlinkSync(req.files.picture2[0].path);
+    }
+    if (req.files.picture3) {
+      fs.unlinkSync(req.files.picture3[0].path);
+    }
+    if (req.files.picture4) {
+      fs.unlinkSync(req.files.picture4[0].path);
+    }
+    if (req.files.picture5) {
+      fs.unlinkSync(req.files.picture5[0].path);
+    }
+  };
+
+  const token = req.headers['x-access-token'];
+  if (!token) {
+    deleteImage();
+    return baseResponse.error(res, 403, 'No access token provided.');
+  }
+
+  const required = [
+    'appointmentId',
+  ];
+  try {
+    required.forEach((requiredItem) => {
+      if (!req.body[requiredItem]) {
+        throw new Error(`Should contain ${requiredItem}`);
+      }
+    });
+  } catch (error) {
+    deleteImage();
+    return baseResponse.error(res, 400, error.message);
+  }
+
+  try {
+    const decodedToken = jwt.decode(token);
+    const user = await User.findOne({
+      where: {
+        uuid: decodedToken.uuid,
+      },
+    });
+
+    const appointment = await Appointment.findOne({
+      where: {
+        id: req.body.appointmentId,
+      },
+      include: {
+        model: Poly,
+        include: {
+          model: User,
+          required: false,
+          where: {
+            roleId: {
+              [Op.eq]: [2],
+            },
+            isActive: {
+              [Op.gte]: [1],
+            },
+          },
+        },
+      },
+    });
+
+    if (!appointment) {
+      deleteImage();
+      return baseResponse.error(res, 404, `Appointment with id [${req.body.appointmentId}] is not found.`);
+    }
+
+    if (!appointment.poly) {
+      deleteImage();
+      return baseResponse.error(res, 404, `Poly for appointment id [${appointment.id}] is not found or already deleted.`);
+    }
+
+    if (!appointment.poly.user) {
+      deleteImage();
+      return baseResponse.error(res, 404, `Hospital for appointment id [${appointment.id}] and poly id [${appointment.poly.id}] is not found, not active or already deleted.`);
+    }
+
+    const dateToday = new Date().toLocaleDateString('en-CA', {timeZone: process.env.APP_TIMEZONE_STRING});
+    const queueHighestToday = await Queue.findOne({
+      where: {
+        appointmentId: appointment.id,
+        date: dateToday,
+      },
+      order: [
+        ['queue', 'DESC'],
+      ],
+      attributes: [
+        'queue',
+      ],
+    }) || false;
+
+    if (queueHighestToday.queue >= appointment.poly.capacity) {
+      deleteImage();
+      return baseResponse.error(res, 400, `Appointment with id [${appointment.id}] has reached maximum capacity for today.`);
+    }
+
+    const picture1 = (req.files && req.files.picture1 ? req.files.picture1[0].filename : undefined);
+    const picture2 = (req.files && req.files.picture2 ? req.files.picture2[0].filename : undefined);
+    const picture3 = (req.files && req.files.picture3 ? req.files.picture3[0].filename : undefined);
+    const picture4 = (req.files && req.files.picture4 ? req.files.picture4[0].filename : undefined);
+    const picture5 = (req.files && req.files.picture5 ? req.files.picture5[0].filename : undefined);
+
+    await Queue.create({
+      userId: user.id,
+      appointmentId: appointment.id,
+      date: dateToday,
+      isAssurance: req.body.isAssurance,
+      picture1: picture1,
+      picture2: picture2,
+      picture3: picture3,
+      picture4: picture4,
+      picture5: picture5,
+      queueStatusId: 0,
+    });
+
+    return baseResponse.ok(res, {
+      message: `Queue has been created successfully, please wait the hospital to verify your request.`,
+    });
+  } catch (error) {
+    deleteImage();
     return baseResponse.error(res, 500, error.message);
   }
 };
--- src/backend/middleware/upload.js
+++ src/backend/middleware/upload.js
@@ -53,6 +53,31 @@ const polyPictureUploadSetting = {
   },
 };
 
+const queuePicturesUploadSetting = {
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      const dir = path.join(__dirname, '../public/uploads/queues/pictures');
+      mkdirp.sync(dir);
+      cb(null, dir);
+    },
+    filename: (req, file, cb) => {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) + path.extname(file.originalname);
+      cb(null, uniqueSuffix);
+    },
+  }),
+
+  fileFilter: (req, file, cb) => {
+    if (!file.mimetype.includes('image')) {
+      return cb(new Error(`Field [${file.fieldname}] must be an image.`), false);
+    }
+    return cb(null, true);
+  },
+
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5 MB
+  },
+};
+
 const userProfileUpload = multer({
   storage: userProfileUploadSetting.storage,
   fileFilter: userProfileUploadSetting.fileFilter,
@@ -65,9 +90,31 @@ const polyPictureUpload = multer({
   limits: polyPictureUploadSetting.limits,
 }).single('picture');
 
+const queuePicturesUpload = multer({
+  storage: queuePicturesUploadSetting.storage,
+  fileFilter: queuePicturesUploadSetting.fileFilter,
+  limits: queuePicturesUploadSetting.limits,
+}).fields([{
+  name: 'picture1',
+  maxCount: 1,
+}, {
+  name: 'picture2',
+  maxCount: 1,
+}, {
+  name: 'picture3',
+  maxCount: 1,
+}, {
+  name: 'picture4',
+  maxCount: 1,
+}, {
+  name: 'picture5',
+  maxCount: 1,
+}]);
+
 const upload = {
   userProfileUpload: userProfileUpload,
   polyPictureUpload: polyPictureUpload,
+  queuePicturesUpload: queuePicturesUpload,
 };
 
 module.exports = upload;
