e62c4b6900eb5ebc1d84fa9ff11f08aa8bad6546
TheSkinnyRat <purwa.sabrang@gmail.com>
2021-12-16 01:22:51 +0700

    code improvement

--- src/backend/controllers/user.controller.js
+++ src/backend/controllers/user.controller.js
@@ -126,6 +126,149 @@ exports.deleteProfile = async (req, res) => {
   }
 };
 
+exports.addQueue = async (req, res) => {
+  const deleteImage = () => {
+    if (req.files) {
+      if (req.files.picture1) {
+        fs.unlinkSync(req.files.picture1[0].path);
+      }
+      if (req.files.picture2) {
+        fs.unlinkSync(req.files.picture2[0].path);
+      }
+      if (req.files.picture3) {
+        fs.unlinkSync(req.files.picture3[0].path);
+      }
+      if (req.files.picture4) {
+        fs.unlinkSync(req.files.picture4[0].path);
+      }
+      if (req.files.picture5) {
+        fs.unlinkSync(req.files.picture5[0].path);
+      }
+    }
+  };
+
+  const token = req.headers['x-access-token'];
+  if (!token) {
+    deleteImage();
+    return baseResponse.error(res, 403, 'No access token provided.');
+  }
+
+  const required = [
+    'appointmentId',
+  ];
+  try {
+    required.forEach((requiredItem) => {
+      if (!req.body[requiredItem]) {
+        throw new Error(`Should contain ${requiredItem}`);
+      }
+    });
+  } catch (error) {
+    deleteImage();
+    return baseResponse.error(res, 400, error.message);
+  }
+
+  try {
+    const decodedToken = jwt.decode(token);
+    const user = await User.findOne({
+      where: {
+        uuid: decodedToken.uuid,
+      },
+    });
+
+    const appointment = await Appointment.findOne({
+      where: {
+        id: req.body.appointmentId,
+      },
+      include: {
+        model: Poly,
+        include: {
+          model: User,
+          required: false,
+          where: {
+            roleId: {
+              [Op.eq]: [2],
+            },
+            isActive: {
+              [Op.gte]: [1],
+            },
+          },
+        },
+      },
+    });
+
+    if (!appointment) {
+      deleteImage();
+      return baseResponse.error(res, 404, `Appointment with id [${req.body.appointmentId}] is not found.`);
+    }
+
+    if (!appointment.poly) {
+      deleteImage();
+      return baseResponse.error(res, 404, `Poly for appointment id [${appointment.id}] is not found or already deleted.`);
+    }
+
+    if (!appointment.poly.user) {
+      deleteImage();
+      return baseResponse.error(res, 404, `Hospital for appointment id [${appointment.id}] and poly id [${appointment.poly.id}] is not found, not active or already deleted.`);
+    }
+
+    const dateToday = new Date().toLocaleDateString('en-CA', {timeZone: process.env.APP_TIMEZONE_STRING});
+    const dayToday = new Date(dateToday).getDay();
+    if (dayToday !== 0) {
+      if (dayToday !== appointment.day) {
+        return baseResponse.error(res, 400, `Current date [${dayToday}] is not valid for appointment date [${appointment.id}] for appointment id [${appointment.id}].`);
+      }
+    } else {
+      if (dayToday !== appointment.day-7) {
+        return baseResponse.error(res, 400, `Current date [${dayToday}] is not valid for appointment date [${appointment.day}] for appointment id [${appointment.id}].`);
+      }
+    }
+
+    const queueHighestToday = await Queue.findOne({
+      where: {
+        appointmentId: appointment.id,
+        date: dateToday,
+      },
+      order: [
+        ['queue', 'DESC'],
+      ],
+      attributes: [
+        'queue',
+      ],
+    }) || false;
+
+    if (queueHighestToday.queue >= appointment.poly.capacity) {
+      deleteImage();
+      return baseResponse.error(res, 400, `Appointment with id [${appointment.id}] has reached maximum capacity for today.`);
+    }
+
+    const picture1 = (req.files && req.files.picture1 ? req.files.picture1[0].filename : undefined);
+    const picture2 = (req.files && req.files.picture2 ? req.files.picture2[0].filename : undefined);
+    const picture3 = (req.files && req.files.picture3 ? req.files.picture3[0].filename : undefined);
+    const picture4 = (req.files && req.files.picture4 ? req.files.picture4[0].filename : undefined);
+    const picture5 = (req.files && req.files.picture5 ? req.files.picture5[0].filename : undefined);
+
+    await Queue.create({
+      userId: user.id,
+      appointmentId: appointment.id,
+      date: dateToday,
+      isAssurance: req.body.isAssurance,
+      picture1: picture1,
+      picture2: picture2,
+      picture3: picture3,
+      picture4: picture4,
+      picture5: picture5,
+      queueStatusId: 0,
+    });
+
+    return baseResponse.ok(res, {
+      message: `Queue has been created successfully, please wait the hospital to verify your request.`,
+    });
+  } catch (error) {
+    deleteImage();
+    return baseResponse.error(res, 500, error.message);
+  }
+};
+
 exports.getAllQueue = async (req, res) => {
   const token = req.headers['x-access-token'];
 
@@ -482,149 +625,6 @@ exports.getByDateQueue = async (req, res) => {
       queues: queues,
     });
   } catch (error) {
-    return baseResponse.error(res, 500, error.message);
-  }
-};
-
-exports.addQueue = async (req, res) => {
-  const deleteImage = () => {
-    if (req.files) {
-      if (req.files.picture1) {
-        fs.unlinkSync(req.files.picture1[0].path);
-      }
-      if (req.files.picture2) {
-        fs.unlinkSync(req.files.picture2[0].path);
-      }
-      if (req.files.picture3) {
-        fs.unlinkSync(req.files.picture3[0].path);
-      }
-      if (req.files.picture4) {
-        fs.unlinkSync(req.files.picture4[0].path);
-      }
-      if (req.files.picture5) {
-        fs.unlinkSync(req.files.picture5[0].path);
-      }
-    }
-  };
-
-  const token = req.headers['x-access-token'];
-  if (!token) {
-    deleteImage();
-    return baseResponse.error(res, 403, 'No access token provided.');
-  }
-
-  const required = [
-    'appointmentId',
-  ];
-  try {
-    required.forEach((requiredItem) => {
-      if (!req.body[requiredItem]) {
-        throw new Error(`Should contain ${requiredItem}`);
-      }
-    });
-  } catch (error) {
-    deleteImage();
-    return baseResponse.error(res, 400, error.message);
-  }
-
-  try {
-    const decodedToken = jwt.decode(token);
-    const user = await User.findOne({
-      where: {
-        uuid: decodedToken.uuid,
-      },
-    });
-
-    const appointment = await Appointment.findOne({
-      where: {
-        id: req.body.appointmentId,
-      },
-      include: {
-        model: Poly,
-        include: {
-          model: User,
-          required: false,
-          where: {
-            roleId: {
-              [Op.eq]: [2],
-            },
-            isActive: {
-              [Op.gte]: [1],
-            },
-          },
-        },
-      },
-    });
-
-    if (!appointment) {
-      deleteImage();
-      return baseResponse.error(res, 404, `Appointment with id [${req.body.appointmentId}] is not found.`);
-    }
-
-    if (!appointment.poly) {
-      deleteImage();
-      return baseResponse.error(res, 404, `Poly for appointment id [${appointment.id}] is not found or already deleted.`);
-    }
-
-    if (!appointment.poly.user) {
-      deleteImage();
-      return baseResponse.error(res, 404, `Hospital for appointment id [${appointment.id}] and poly id [${appointment.poly.id}] is not found, not active or already deleted.`);
-    }
-
-    const dateToday = new Date().toLocaleDateString('en-CA', {timeZone: process.env.APP_TIMEZONE_STRING});
-    const dayToday = new Date(dateToday).getDay();
-    if (dayToday !== 0) {
-      if (dayToday !== appointment.day) {
-        return baseResponse.error(res, 400, `Current date [${dayToday}] is not valid for appointment date [${appointment.id}] for appointment id [${appointment.id}].`);
-      }
-    } else {
-      if (dayToday !== appointment.day-7) {
-        return baseResponse.error(res, 400, `Current date [${dayToday}] is not valid for appointment date [${appointment.day}] for appointment id [${appointment.id}].`);
-      }
-    }
-
-    const queueHighestToday = await Queue.findOne({
-      where: {
-        appointmentId: appointment.id,
-        date: dateToday,
-      },
-      order: [
-        ['queue', 'DESC'],
-      ],
-      attributes: [
-        'queue',
-      ],
-    }) || false;
-
-    if (queueHighestToday.queue >= appointment.poly.capacity) {
-      deleteImage();
-      return baseResponse.error(res, 400, `Appointment with id [${appointment.id}] has reached maximum capacity for today.`);
-    }
-
-    const picture1 = (req.files && req.files.picture1 ? req.files.picture1[0].filename : undefined);
-    const picture2 = (req.files && req.files.picture2 ? req.files.picture2[0].filename : undefined);
-    const picture3 = (req.files && req.files.picture3 ? req.files.picture3[0].filename : undefined);
-    const picture4 = (req.files && req.files.picture4 ? req.files.picture4[0].filename : undefined);
-    const picture5 = (req.files && req.files.picture5 ? req.files.picture5[0].filename : undefined);
-
-    await Queue.create({
-      userId: user.id,
-      appointmentId: appointment.id,
-      date: dateToday,
-      isAssurance: req.body.isAssurance,
-      picture1: picture1,
-      picture2: picture2,
-      picture3: picture3,
-      picture4: picture4,
-      picture5: picture5,
-      queueStatusId: 0,
-    });
-
-    return baseResponse.ok(res, {
-      message: `Queue has been created successfully, please wait the hospital to verify your request.`,
-    });
-  } catch (error) {
-    deleteImage();
     return baseResponse.error(res, 500, error.message);
   }
 };
